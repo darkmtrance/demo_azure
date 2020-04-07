ef823eb40a58e48b4094623aee6f4c9956b9f983
darkmtrance <pulser2310@gmail.com>
2020-04-07 11:14:58 -0500

    fix

--- prueba01/storageaccount.tf
+++ prueba01/storageaccount.tf
@@ -11,8 +11,8 @@ resource "azurerm_resource_group" "test" {
 }
 resource "azurerm_storage_account" "stgid" {
   name                     = "mtm2020"
-  resource_group_name      = "${azurerm_resource_group.test.name}"
-  location                 = "${azurerm_resource_group.test.location}"
+  resource_group_name      = "azurerm_resource_group.test.name"
+  location                 = "azurerm_resource_group.test.location"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
